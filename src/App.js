fc7e4978256bdbe68d6afb367f4b3398b72eb378
sigvant <bnsviv@gmail.com>
2021-10-04 17:22:08 -0300

    final lesson on memo optimization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 // import { ChildOne } from './components/Optimization/ChildOne/';
-import { GrandParent } from './components/Optimization/GrandParent';
+// import { GrandParent } from './components/Optimization/GrandParent';
+import ParentTwo from './components/Optimization/ParentTwo';
 // import ParentOne from './components/Optimization/ParentOne';
 
 
 function App() {
   return (
     <div className="App">
-      <GrandParent/>
+      <ParentTwo/>
     </div>
   );
 }
---
+++ src/components/Optimization/ParentTwo.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { MemoizedChildTwo } from './ChildTwo'
+
+const ParentTwo = ({children}) => {
+
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState(`Vishwas`)
+
+    console.log(`ParentTwo Render`)
+    return (
+        <div>
+            <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>   
+            <button onClick={() => setName(`Codevolution`)}>Change Name</button>
+            <MemoizedChildTwo name={name}/>      
+            
+        </div>
+    )
+}
+
+export default ParentTwo
