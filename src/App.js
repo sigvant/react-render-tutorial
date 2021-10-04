7ebddfce28f61e490e3ac25e172a3949852af00f
sigvant <bnsviv@gmail.com>
2021-10-04 09:35:21 -0300

    end of useReducer render mechanisms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { UseState } from './components/UseState/UseState';
+import { UseReducer } from './components/UseReducer/UseReducer';
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      <UseReducer/>
     </div>
   );
 }
